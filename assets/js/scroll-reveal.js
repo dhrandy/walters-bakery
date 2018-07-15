771d15d7244c1a0a727c7f27f5979a401afbeb54
dhrandy <dhrandy@gmail.com>
2018-07-14 21:37:59 -0400

    uploading bakery site

---
+++ assets/js/scroll-reveal.js
@@ -0,0 +1,20 @@
+window.sr = ScrollReveal();
+sr.reveal('.center-btn', {
+    duration: 2000,
+    origin: 'bottom'
+})
+sr.reveal('#logo', {
+    duration: 1000,
+    origin: 'left',
+    distance: '200px'
+})
+sr.reveal('.stack-of-cookies', {
+    duration: 1000,
+    origin: 'top',
+    distance: '200px'
+})
+sr.reveal('#pie', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '200px'
+})
